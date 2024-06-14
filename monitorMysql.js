0087b273153d1f8171cf727ed5bf79da1a074f7c
Anders Kirkeby <anders@akirkeby.dk>
2024-06-14 06:05:29 +0200

    Added filetype

--- monitorMysql.js
+++ monitorMysql.js
@@ -138,9 +138,19 @@ const visitedUrls = new Set()
 const errorLog = []
 const videoFormats = ['.mp4', '.avi', '.flv', '.mov', '.wmv', '.webm']
 const fileFormats = ['.pdf', '.xlsx', '.csv', '.docx', '.doc', '.ppt', '.pptx', '.xls']
+const webFormats = ['.css', '.js', '.html', '.htm', '.json', '.xml', '.svg', '.txt', '.md', '.php', '.asp', '.aspx', '.jsp', '.py', '.rb', '.java', '.c', '.cpp', '.cs', '.ts', '.jsx', '.tsx', '.vue', '.svelte', '.go', '.rs', '.swift', '.kt', '.dart', '.pl', '.sh', '.bat', '.ps1', '.psm1', '.psd1', '.ps1xml']
 
 const isVideoFormat = url => videoFormats.some(ext => url.endsWith(ext))
 const isFileFormat = url => fileFormats.some(ext => url.endsWith(ext))
+const getFileFormat = (url, entryType) => {
+    
+    const extention = url.split('.').pop()
+    console.log('getFileFormat', { url, extention, entryType })
+
+    const formats = [...webFormats, ...fileFormats, ...videoFormats]
+
+    return formats.some(ext => url.endsWith(ext)) ? extention : 'html'
+}
 
 const pageVisit = async (browser, websiteId, tenantId, siteName, url, measurementId, depth = 0, maxDepth = -1) => {
     const originalUrl = url
@@ -190,13 +200,20 @@ const pageVisit = async (browser, websiteId, tenantId, siteName, url, measuremen
             const type = response.headers()['content-type']
             const encoding = response.headers()['content-encoding']
             const status = response.status()
+            
+            if (status == 404) {
+                // no need to handle non-existing resources
+                console.log('!!!> 404 found', { requestUrl, status })
+                return
+            }
+            
             const headerSize = Buffer.byteLength(JSON.stringify(response.headers()))
             const encodedBodySize = parseInt(response.headers()['content-length']) || 0
-            
             const decodedBodySize = (isVideoFormat(requestUrl) || isFileFormat(requestUrl)) ? encodedBodySize : Buffer.byteLength(await response.buffer())
     
             requestPayloads.push({ requestUrl, pageUrl: url, status, type, headerSize, encodedBodySize, decodedBodySize, encoding })
-            //console.log('done processing response', requestUrl, {decodedBodySize, requestPayloads})
+            console.log('Done with response', requestUrl, {decodedBodySize, requestPayloads})
+
         } catch (error) {
             console.error('Error: Could not get response for request. Skipping', url)
             console.error(error)
@@ -236,21 +253,27 @@ const pageVisit = async (browser, websiteId, tenantId, siteName, url, measuremen
     const perfEntries = JSON.parse(pageData).filter(x => !['paint', 'visibility-state','long-animation-frame'].includes(x.entryType));
 
     // map collected payload sizes onto their matching perf entries
-    perfEntries.map(x => {
+    const entries = [...perfEntries]
+    entries.map(x => {
         x.pageUrl = url
+        x.type = getFileFormat(x.name, x.entryType)
 
         // for files opened in a new tab, we still need them to appear as regular file resources and not navigation items
-        if (x.name === 'about:blank' && isFileFormat(x.pageUrl)) {
-            x.name = x.pageUrl
-            x.entryType = 'resource'
-            x.initiatorType = 'file'
-        }
+        // if (x.name === 'about:blank' && isFileFormat(x.pageUrl)) {
+        //     x.name = x.pageUrl
+        //     x.entryType = 'resource'
+        //     x.initiatorType = 'file'
+        // }
 
-        const payload = requestPayloads.find(y => y.requestUrl == x.pageUrl)
+        // TODO: for favicon.ico, we need to find the correct pageUrl, currently it finds the PDF page
+        const payload = requestPayloads.find(y => y.requestUrl == x.name)
         if (!payload) return
         
         const { headerSize, decodedBodySize, encodedBodySize, encoding } = payload
 
+        // if (isFileFormat(x.name)) {
+        //     x.type = payload.type
+        // }
         x.headerSize = headerSize || 0
         x.encodedBodySize = encoding ? encodedBodySize : decodedBodySize
         x.decodedBodySize = decodedBodySize || 0
@@ -267,8 +290,8 @@ const pageVisit = async (browser, websiteId, tenantId, siteName, url, measuremen
     })
 
     // send the data to the database
-    //console.log('sending data to mysql', { measurementId, perfEntries, requestPayloads })
-    await sendMysql2(websiteId, tenantId, measurementId, perfEntries)
+    console.log('sending data to mysql', { measurementId, perfEntries, requestPayloads, result: entries })
+    await sendMysql2(websiteId, tenantId, measurementId, entries)
 
     // if maxDepth is set and we're at the limit, don't go any deeper
     if (maxDepth > -1 && depth > maxDepth) {
@@ -296,11 +319,11 @@ const pageVisit = async (browser, websiteId, tenantId, siteName, url, measuremen
 
 const runSite = async (siteId) => {
     //const url = args[0] || 'http://localhost:4001'
-    console.log('running site', siteId)
+    console.log('Running site', siteId)
     const siteResult = await mysql.query(`SELECT websiteId, name, url FROM Websites WHERE websiteId = ${siteId}`)
     const { name, url } = siteResult[0]
     const { insertId: measurementId } = await mysql.query(`INSERT INTO Measurements (tenantId, websiteId, modelId) VALUES (1, ${siteId}, 1);`)
-    console.log('inserted measurement', { measurementId, name, url })
+    console.log('Created measurement', { measurementId, name, url })
 
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch({
@@ -328,17 +351,15 @@ export {
     console.log('-------------------------------------')
 
     const args = process.argv.slice(2)
-    if (process.argv.length < 3) {
+    if (args.length == 0) {
         console.log('Ran monitor, but no arguments. Exitting.')
         return
     }
+
     const promises = []
-
-    if (args.length > 0) {
-        args.forEach(url => promises.push(runSite(url)))
-    }
-
+    args.forEach(url => promises.push(runSite(url)))
     await Promise.all(promises)
+    
     console.log('Done running', promises.length, 'sites')
     process.exit(0)
 })()
